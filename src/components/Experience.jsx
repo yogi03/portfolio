32a962c39d2cf1b0ff88d54bccee72fe15cd0da0
YogendraChaurasiya <yogendra062003@outlook.com>
2024-06-02 01:23:44 +0530

    Fix string concatenation warning in Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -99,8 +99,8 @@ const Experience = () => {
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
           {
             techs.map(({id, src, title, style})=> (
-              <div key={id} className={'shadow-md hover:scale-105 duration-500 py-2 rounded-lg' + ' ' + style}>
-                <img src={src} alt="HTML_img_logo" className='w-20 mx-auto' />
+              <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                <img src={src} alt={`${title}_img_logo`} className='w-20 mx-auto' />
                 <p className='mt-4'>{title}</p>
               </div>
             ))

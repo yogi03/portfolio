3d3494f7401e3b77d6629b06d5270578d73978e8
YOGENDRA CHAURASIYA <Yogendra062003@outlook.com>
2024-09-05 18:13:39 +0530

    Technologies Logo

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -108,7 +108,7 @@ const Experience = () => {
           <p className="py-6">These are the technologies I've worked in</p>
         </div>
 
-        <div className="w-full grid grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}

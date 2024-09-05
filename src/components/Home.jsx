51f4e2801cae332942fd3cd4d5937a5bcc944e81
YOGENDRA CHAURASIYA <Yogendra062003@outlook.com>
2024-09-05 13:45:25 +0530

    Portfolio Links Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="Home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Web Developer</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Developer</h2>
                 <p className='text-gray-500 py-4 mx-w-md'>Ready to bring innovative solutions to your team, leveraging my expertise in front-end development, project management, and strategic technology implementation to drive impactful results and elevate your digital presence. Connect with me today to transform your vision into reality.</p>
                 <div>
                     <Link to='Portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>Portfolio 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -18,10 +18,10 @@ const Portfolio = () => {
             <div className='shadow-md shadow-gray-600 rounded-lg'>
                     <img src={bi} alt="BI" className='rounded-md duration-200 hover:scale-105'/>
                     <div className='flex items-center justify-center'>
-                        <a href="https://github.com/BinariIntelligence/BinariIntelligenceWebsite/tree/Yogendra" target="_blank" rel="noopener noreferrer" className='w-1/2'>
+                        <a href="https://binariintelligence.in/" target="_blank" rel="noopener noreferrer" className='w-1/2'>
                             <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
                         </a>
-                        <a href="https://binariintelligence.in/" target="_blank" rel="noopener noreferrer" className='w-1/2'>
+                        <a href="https://github.com/BinariIntelligence/BinariIntelligenceWebsite/tree/Yogendra" target="_blank" rel="noopener noreferrer" className='w-1/2'>
                             <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
                         </a>
                     </div>

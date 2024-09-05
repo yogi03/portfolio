624fa83afaf91e033370cca56393f7b2fa0c8b23
YOGENDRA CHAURASIYA <Yogendra062003@outlook.com>
2024-09-05 17:46:20 +0530

    Fix Personal Image Size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
 
   return (
     <div name="Home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row gap-8 md:gap-0 sm:gap-0">
         <div className='flex flex-col justify-center h-full'>
           <h2 className='text-4xl sm:text-7xl font-bold text-white'>
             I'm a <span className='fade-in-out'>{currentWord}</span>

17a44781bc0342ffea196bbb6ffd7ce126513408
yogi03 <yogendrachaurasiya30@gmail.com>
2025-06-26 13:17:43 +0530

    Portfolio Heading Overlapping fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='About' className='w-full max-h-fit bg-[#FFFFF0] text-gray-800 pt-20 pb-8 sm:pt-0'>
+    <div name='About' className='w-full max-h-fit bg-[#FFFFF0] text-gray-800 pt-20 pb-0 sm:pb-8 sm:pt-0'>
         <div className='max-w-screen-lg px-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-1'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-800'>ABOUT</p>
